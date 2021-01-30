51e8fabd6d3214bf5427092040f8183f2bdbbaa4
yr0418 <1933352594@qq.com>
2021-01-30 14:15:29 +0800

    引入typing模块，修正错误

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     solution = Solution()  # 调用类时，先进行类的初始化
     nums = [2, 7, 11, 15]
     target = 9
-    print(solution.twoSum_2(nums, target))
+    print(solution.twoSum_1(nums, target))
--- solution.py
+++ solution.py
@@ -3,8 +3,11 @@
 # @Description：算法
 # @Author : 杨睿
 # @Time : 2021-01-14 23:21
+from typing import *
+
+
 class Solution:
-    def twoSum_1(self, nums: list[int], target: int) -> list[int]:
+    def twoSum_1(self, nums: List[int], target: int) -> List[int]:
         """
         :type nums: List[int]
         :type target: int
@@ -12,11 +15,11 @@ class Solution:
         :description：两数之和，暴力破解
         """
         for index1 in range(len(nums)):
-            for index2 in range(index1+1, len(nums)):
+            for index2 in range(index1 + 1, len(nums)):
                 if nums[index1] + nums[index2] == target:
                     return [index1, index2]
 
-    def twoSum_2(self, nums: list[int], target: int) -> list[int]:
+    def twoSum_2(self, nums: List[int], target: int) -> List[int]:
         """
         :type nums: List[int]
         :type target: int
@@ -29,3 +32,20 @@ class Solution:
                 return [hashtable[target - num], i]
             hashtable[nums[i]] = i  # nums 中的数据进字典。nums[i] 为键，i为值
         return []
+
+    def reverse(self, x: int) -> int:
+        """
+        :type x: int
+        :rtype: int
+        :description：整数反转
+        """
+        result = 0
+        while x != 0:
+            pop = x % 10
+            x = x / 10
+            result = result * 10 + pop
+            if -pow(2, 31) <= result <= pow(2, 31) - 1:
+                continue
+            else:
+                return 0
+        return result
