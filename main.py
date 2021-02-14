5b6ad4a7125eb5b971788d6326f21b1133bc3bf4
yr0418 <1933352594@qq.com>
2021-02-14 15:44:18 +0800

    简单算法14：加一

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from solution_middle import Solution as SolutionMiddle
 
 if __name__ == '__main__':
     solution = SolutionSimple()  # 调用类时，先进行类的初始化
-    s = "hello world    "
-    print(solution.lengthOfLastWord(s))
+    digits = [0, 1, 2]
+    print(solution.plusOne(digits))
--- solution_simple.py
+++ solution_simple.py
@@ -476,4 +476,19 @@ class Solution:
                 j -= 1
         return length
 
+    def plusOne(self, digits: List[int]) -> List[int]:
+        """
+        :description: 加一
+        :param digits: list
+        :return: list
+        """
+        for i in range(len(digits)-1, -1, -1):
+            digits[i] += 1
+            if digits[i] != 10:
+                break
+            else:
+                digits[i] = 0
+                if i == 0:
+                    digits.insert(0, 1)
+        return digits
 
